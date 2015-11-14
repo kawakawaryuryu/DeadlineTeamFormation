dfe4d13be37e453056d2b5bb6941bc139c767460
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2015-11-15 03:32:32 +0900

    run.shを修正

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain 1 learning estimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain 2 noLearning estimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain 3 learning noEstimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain 4 random noEstimation &
+java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 1 learning estimation &
+java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 2 noLearning estimation &
+java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 3 learning noEstimation &
+java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 4 random noEstimation &
