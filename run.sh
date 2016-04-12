b1de64d039c52e68f429997fefb10416c8aed382
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-04-12 18:55:23 +0900

    run.shを修正

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 1 learning estimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 2 noLearning estimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 3 learning noEstimation &
-java -Xms4096m -Xmx4096m -classpath bin fixed.main.MainMain debug 4 random noEstimation &
+memory=1024
+java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 1 learning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 2 noLearning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 3 learning noEstimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 4 random noEstimation &
