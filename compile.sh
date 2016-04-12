56b4bb728489b761bfd86cb96fb5c3c14a41aee1
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-04-13 01:20:34 +0900

    コンパイル用のシェルスクリプト追加

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+compile() {
+	for file in $1/*
+	do
+		if [ -d $file ]; then
+			[[ $file = */test ]] && continue 
+			compile $file
+		elif [ -f $file ]; then
+			[[ $file = *.java ]] && javac -classpath bin -d bin $file
+		fi
+	done
+}
+
+compile "$PWD/src/fixed"
