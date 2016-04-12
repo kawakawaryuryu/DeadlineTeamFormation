dc8bb0b382ba6181f20edf532abe8a770da9e47e
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-04-12 19:18:25 +0900

    run.shのメモリ量を修正

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-memory=1024
+memory=4096
 java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 1 learning estimation &
 java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 2 noLearning estimation &
 java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 3 learning noEstimation &
