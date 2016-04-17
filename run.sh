d58916d1ba3ec954f464e5014066dbbd69627249
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2016-04-18 02:45:23 +0900

    run.shを修正

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 memory=4096
-java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 1 learning estimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 2 noLearning estimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 3 learning noEstimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin fixed.main.MainMain debug 4 random noEstimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin main.MainMain debug 1 learning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin main.MainMain debug 2 noLearning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin main.MainMain debug 3 learning noEstimation &
+java -Xms${memory}m -Xmx${memory}m -classpath bin main.MainMain debug 4 random noEstimation &
