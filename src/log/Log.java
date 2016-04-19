551f76ae4b859fc809fd5ddcc353854e95d39711
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2016-04-19 23:16:57 +0900

    log4j2の設定ファイルとクラスを追加

---
+++ src/log/Log.java
@@ -0,0 +1,11 @@
+package log;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+public class Log {
+
+	public static Logger log = LogManager.getLogger(Log.class.getName());
+
+}
