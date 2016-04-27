c8082a84b2425ef4beea520822e288eec5c41216
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-04-27 18:49:12 +0900

    ログクラスにメソッド追加 / ログインスタンスの移動

--- src/config/Configuration.java
+++ src/config/Configuration.java
@@ -1,13 +1,11 @@
 package config;
 
-import log.Log;
 import log.logger.Type;
 
 
 public class Configuration {
 	
-	public static String path = "log/debug.log";
-	public static final Type TYPE = Type.FILE_DEBUG;
+	public static final String LOG_PATH = "log/debug.log";
+	public static final Type LOG_TYPE = Type.FILE_DEBUG;
 
-	public static Log log = new Log(TYPE, path);
 }
--- src/log/Log.java
+++ src/log/Log.java
@@ -1,5 +1,6 @@
 package log;
 
+import config.Configuration;
 import log.logger.FileLogger;
 import log.logger.Logger;
 import log.logger.NoLogger;
@@ -8,7 +9,9 @@ import log.logger.Type;
 
 public class Log {
 
-	private Logger logger;
+	private static Logger logger;
+
+	public static Log log = new Log(Configuration.LOG_TYPE, Configuration.LOG_PATH);
 
 	public Log(Type type, String path) {
 		if (type == Type.FILE_DEBUG) {
@@ -27,9 +30,21 @@ public class Log {
 	public void debug(String msg) {
 		logger.debug(msg);
 	}
+	
+	public void debug(Object obj) {
+		logger.debug(obj.toString());
+	}
 
 	public void debugln(String msg) {
 		logger.debugln(msg);
+	}
+	
+	public void debugln() {
+		logger.debugln("");
+	}
+
+	public void debugln(Object obj) {
+		logger.debugln(obj.toString());
 	}
 
 	public void close() {
