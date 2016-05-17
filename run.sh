41ebd595e3f228921dc9182d7acb35ec4d656f38
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-05-18 01:25:44 +0900

    jarに関するパス等修正

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 memory=4096
-java -Xms${memory}m -Xmx${memory}m -classpath bin main.Main debug 1 learning estimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin main.Main debug 2 noLearning estimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin main.Main debug 3 learning noEstimation &
-java -Xms${memory}m -Xmx${memory}m -classpath bin main.Main debug 4 random noEstimation &
+java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 1 learning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 2 noLearning estimation &
+java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 3 learning noEstimation &
+java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 4 random noEstimation &
