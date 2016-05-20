43ebdd8a49d16d8cf9978d4d0a7f591dca1afe97
川口 竜太郎 <kawaguchiryuutarou@kawaguchi-no-MacBook-Air.local>
2016-05-20 19:02:07 +0900

    結果ファイルの保存先変更によるdiff.shの修正

--- diff.sh
+++ diff.sh
@@ -22,44 +22,73 @@ directory2=$6
 date2=$7
 time2=$8
 
-path1="${HOME}/Dropbox/research/${type1}/${date1}/${directory1}"
-path2="${HOME}/Dropbox/research/${type2}/${date2}/${directory2}"
+cmn_path="${HOME}/Box Sync/Personal/research"
+path1="${cmn_path}/${type1}/${date1}/${directory1}"
+path2="${cmn_path}/${type2}/${date2}/${directory2}"
 
 tmp="${PWD}/tmp"
+
+trim_path="${HOME}/BoxSync/Personal/research"
+
+
+
+trim() {
+	local word=$1
+	local trimed=`echo "$word" | sed -e "s/ //"`
+	echo "$trimed"
+}
+
+restore() {
+	local word=$1
+	local origin=`echo "$word" | sed -e "s%$trim_path%$cmn_path%"`
+	echo "$origin"
+}
+
+
 
 get_dir() {
 	local dir=$1
 	local time=$2
-	for file in $dir/*
+	for file in "$dir"/*
 	do
-		if [ -d $file ]; then
-			get_dir $file $time
-		elif [ -f $file ]; then
-			if [[ $file = *.csv ]] && [ `echo $file | grep $time` ]; then
-				f=`basename $file`
-				echo ${file%/${f}}
-				break
+		if [ -d "$file" ]; then
+			get_dir "$file" $time
+		elif [ -f "$file" ]; then
+			if [[ "$file" = *.csv ]]; then
+				# ファイルのパスから一時的にスペースを除く
+				local file_tmp=`trim "$file"`
+				if [ `echo "$file_tmp" | grep "$time"` ]; then
+					f=`basename "$file_tmp"`
+					echo "$file_tmp" | sed -e "s%/$f%%"
+					break
+				fi
 			fi
 		fi
 	done
 }
 
 search() {
-	for dir in $path1/*
+	for dir in "$path1"/*
 	do
-		local d=`basename $dir`
+		local d=`basename "$dir"`
 
 		# diffを取るファイルを含むディレクトリを取得
+		# 戻り値はスペースを覗いたパス
 		local ret1=(`get_dir "$path1/$d" $time1`)
 		local ret2=(`get_dir "$path2/$d" $time2`)
+		#get_dir "$path1/$d" $time1
 
 
 		for((i=0;i<${#ret1[*]};i++))
 		do
 
 			# ファイル名に指定の時間が含まれるもののみを取得
-			files1=(`validate_file ${ret1[$i]} $time1`)
-			files2=(`validate_file ${ret2[$i]} $time2`)
+			# パスをスペースのあるものに復元
+			local f1=`restore "${ret1[$i]}"`
+			local f2=`restore "${ret2[$i]}"`
+			# 戻り値はスペースを除いたパス
+			files1=(`validate_file "$f1" $time1`)
+			files2=(`validate_file "$f2" $time2`)
 
 			# 要素数が最小の方の要素数を取得
 			end=`min ${#files1[*]} ${#files2[*]}`
@@ -67,13 +96,16 @@ search() {
 			for((j=0;j<$end;j++))
 			do
 				# 比較するファイルが適切かチェック
-				success=`check ${files1[$j]} ${files2[$j]}`
+				success=`check "${files1[$j]}" "${files2[$j]}"`
 				if [ $success -eq 0 ]; then
-					diff_files ${files1[$j]} ${files2[$j]}
+					# パスをスペースのあるものに復元
+					f1=`restore "${files1[$j]}"`
+					f2=`restore "${files2[$j]}"`
+					diff_files "$f1" "$f2"
 				else
 					echo "ERROR!!!"
-					echo ${files1[$j]}
-					echo ${files2[$j]}
+					echo "$f1"
+					echo "$f2"
 					break
 				fi
 			done
@@ -85,10 +117,12 @@ search() {
 validate_file() {
 	local dir=$1
 	local time=$2
-	for file in $dir/*
+	for file in "$dir"/*
 	do
-		if [ `echo $file | grep $time` ]; then
-			echo $file
+		# ファイルのパスから一時的にスペースを除く
+		local file_tmp=`trim "$file"`
+		if [ `echo "$file_tmp" | grep $time` ]; then
+			echo "$file_tmp"
 		fi
 	done
 }
@@ -106,8 +140,8 @@ check() {
 	local file2=$2
 
 	# ファイル名からターン数、日付、時間、ディレクトリ名を取り除く
-	local f1=`echo $file1 | sed -e "s%$date1%%" -e "s%$time1%%" -e "s%$directory1%%" | sed -E "s%/[0-9]+t%%"`
-	local f2=`echo $file2 | sed -e "s%$date2%%" -e "s%$time2%%" -e "s%$directory2%%" | sed -E "s%/[0-9]+t%%"`
+	local f1=`echo "$file1" | sed -e "s%$date1%%" -e "s%$time1%%" -e "s%$directory1%%" | sed -E "s%/[0-9]+t%%"`
+	local f2=`echo "$file2" | sed -e "s%$date2%%" -e "s%$time2%%" -e "s%$directory2%%" | sed -E "s%/[0-9]+t%%"`
 
 	if [ $f1 = $f2 ]; then
 		echo 0
@@ -119,25 +153,26 @@ check() {
 diff_files() {
 	local path1=$1
 	local path2=$2
-	local first="/Users/kawaguchiryuutarou/Dropbox/research"
 
-	local tmp_f1="$tmp${path1#${first}}"
-	local tmp_f2="$tmp${path2#${first}}"
+	local tmp_f1=$tmp/`echo "$path1" | sed -e "s%$cmn_path/%%"`
+	local tmp_f2=$tmp/`echo "$path2" | sed -e "s%$cmn_path/%%"`
 
 	# tmpディレクトリ、ファイル作成
-	local f1=`basename $path1`
-	local f2=`basename $path2`
-	mkdir -p ${tmp_f1%${f1}}
-	mkdir -p ${tmp_f2%${f2}}
-	touch $tmp_f1
-	touch $tmp_f2
+	local f1=`basename "$path1"`
+	local f2=`basename "$path2"`
+	local dir1=`echo "$tmp_f1" | sed -e "s%/$f1%%"`
+	local dir2=`echo "$tmp_f2" | sed -e "s%/$f2%%"`
+	mkdir -p "$dir1"
+	mkdir -p "$dir2"
+	touch "$tmp_f1"
+	touch "$tmp_f2"
 	
 	# 文字コードをUTF-8にして別ファイルに書き出し
-	nkf -w $path1 > $tmp_f1
-	nkf -w $path2 > $tmp_f2
+	nkf -w "$path1" > "$tmp_f1"
+	nkf -w "$path2" > "$tmp_f2"
 
 	# diffをとる
-	git diff --no-index $tmp_f1 $tmp_f2
+	git diff --no-index "$tmp_f1" "$tmp_f2"
 }
 
 mkdir $tmp
