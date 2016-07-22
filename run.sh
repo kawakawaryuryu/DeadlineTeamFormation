94f861b39082047b036437a1f1418a71d2c0f1a0
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2016-07-22 12:43:24 +0900

    ヒープのNew領域を指定するようにrun.shを変更

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 memory=2048
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 1 Rational learning estimation BTFM/ED &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 2 Rational noLearning estimation ED &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 3 Rational learning noEstimation BTFM &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 4 Rational random noEstimation Random &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 5 Structured learning estimation BTFM/ED &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 6 Structured noLearning estimation ED &
-java -Xms${memory}m -Xmx${memory}m -classpath 'jar/*:bin' main.Main debug 7 Structured learning noEstimation BTFM &
+new=1024
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 1 Rational learning estimation BTFM/ED &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 2 Rational noLearning estimation ED &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 3 Rational learning noEstimation BTFM &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 4 Rational random noEstimation Random &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 5 Structured learning estimation BTFM/ED &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 6 Structured noLearning estimation ED &
+java -Xms${memory}m -Xmx${memory}m -Xmn${new}m -classpath 'jar/*:bin' main.Main debug 7 Structured learning noEstimation BTFM &
