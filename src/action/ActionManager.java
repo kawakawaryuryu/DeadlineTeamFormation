437ebc9b801b72f7ca4af8e1052a13f3f4ff2174
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2016-10-07 21:28:09 +0900

    タスク返却行動の削除

--- src/action/ActionManager.java
+++ src/action/ActionManager.java
@@ -4,6 +4,4 @@ package action;
 public class ActionManager {
 
 	public static Action toTaskReturnedWaitingStateAction = new ToTaskReturnedWaitingStateAction();
-	public static Action taskReturnAction = new TaskReturnUnmarkedWaitingAction();
-
 }
