756517b6205bd351cba40a3b712cf40b3b32bed9
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2016-11-17 18:05:21 +0900

    サブタスクリソースを固定してサブタスクを生成するSubtaskFactoryを作成

---
+++ src/factory/subtask/FixedRequireSubtaskFactory.java
@@ -0,0 +1,35 @@
+package factory.subtask;
+
+import constant.Constant;
+import task.Subtask;
+
+public class FixedRequireSubtaskFactory implements SubtaskFactory {
+
+	/**
+	 * サブタスクリソースを固定して生成する
+	 * @param deadline
+	 * @return
+	 */
+	@Override
+	public Subtask makeSubtask(int deadline) {
+		int[] require = new int[Constant.RESOURCE_NUM];
+		for(int i = 0; i < require.length; i++){
+			require[i] = Constant.TASK_REQUIRE_MALTIPLE * Constant.TASK_DEADLINE_MULTIPLE * Constant.TASK_REQUIRE_FIXED_VALUE;
+		}
+		return new Subtask(require, deadline);
+	}
+
+	/**
+	 * サブタスクリソースを固定して返す
+	 * @return
+	 */
+	@Override
+	public int[] getSubtaskRequire() {
+		int[] require = new int[Constant.RESOURCE_NUM];
+		for(int i = 0; i < require.length; i++){
+			require[i] = Constant.TASK_REQUIRE_MALTIPLE * Constant.TASK_DEADLINE_MULTIPLE * Constant.TASK_REQUIRE_FIXED_VALUE;
+		}
+		return require;
+	}
+
+}
